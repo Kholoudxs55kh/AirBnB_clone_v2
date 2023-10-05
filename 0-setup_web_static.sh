bcd154bfd6744e3d8378693346a917a79735511e
Kholoudxs55kh <kfattem@gmail.com>
2023-10-04 19:58:09 -0700

    fixing shellzeft bardu

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,13 +5,13 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo echo '<html>
+echo '<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>' > /data/web_static/releases/test/index.html
+</html>' | sudo tee -a /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
@@ -36,4 +36,5 @@ printf %s "server {
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
-      internal;"
+      internal;}
+}"
