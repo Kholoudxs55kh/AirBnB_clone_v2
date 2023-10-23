ab87aad70c9431e4ea9ef19d18dc5903ed82dea1
Kholoudxs55kh <kfattem@gmail.com>
2023-10-22 18:13:55 -0700

    fixing

--- models/state.py
+++ models/state.py
@@ -17,7 +17,7 @@ class State(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """ init state """
         super().__init__(*args, **kwargs)
-    
+
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities():
